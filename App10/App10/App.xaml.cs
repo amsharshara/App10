e06a3c99a6f44b3f832b387139a44d3626428558
test <test@DESKTOP>
2020-11-18 17:18:16 +0300

    1

---
+++ App10/App10/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace App10
+{
+    public partial class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            //scroll to see button when keyboar appears
+         Xamarin.Forms.Application.Current.On<Xamarin.Forms.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust
+               (WindowSoftInputModeAdjust.Resize);
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+     
+    }
+}
